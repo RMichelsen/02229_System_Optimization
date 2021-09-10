45d4c95738ace2438c3f5b4521ba91c333e2c0b0
Erik Thomsen <s184200@dtu.dk>
2021-09-10 12:19:16 +0200

    Created c# project and added gitignore file

---
+++ Exercise/MulticoreProcessorScheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MulticoreProcessorScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
