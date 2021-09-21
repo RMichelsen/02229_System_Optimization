05ae0a98cabf2a6e6b06498eedd4e7048df8f3f1
Erik Thomsen <you@example.com>
2021-09-21 08:25:41 +0200

    made SA return tuple list

--- Exercise/MulticoreProcessorScheduler/SimulatedAnnealing.cs
+++ Exercise/MulticoreProcessorScheduler/SimulatedAnnealing.cs
@@ -8,22 +8,29 @@ namespace SimulatedAnnealing
     class SimulatedAnnealing 
     {
         
-        public Solution FindOptimalSolution(List<Task> tasks, List<Processor> processors) 
+        public List<(decimal,Solution)> FindOptimalSolution(List<Task> tasks, List<Processor> processors) 
 		{
 			// start values
         	decimal T = 10000m;
         	decimal r = 0.003m;
         	
+            var results = new List<(decimal,Solution)>();
+            
 			Solution C = SolutionGenerator.GetInititalSolution(tasks, processors);
+            decimal costC = Cost(C);
+            results.Add((costC, C));
+
+            decimal costNeighbour;
         	Random rnd = new Random();
 
-            var results = new List<Solution>();
             while(T > 1) {
                 Solution neighbourC = SolutionGenerator.GenerateNeighbour(C);
-                if (AccProbability(Cost(C), Cost(neighbourC), T) > rnd.Next(0, 1)) {
+                costC = Cost(C);
+                costNeighbour = Cost(neighbourC);
+                if (AccProbability(costC,costNeighbour, T) > rnd.Next(0, 1)) {
                     C = neighbourC;
                     if (IsSolution(C)) {
-                        results.Add(C);
+                        results.Add((costNeighbour, neighbourC));
                     }
                 }
                 T = T * (1 - r);
