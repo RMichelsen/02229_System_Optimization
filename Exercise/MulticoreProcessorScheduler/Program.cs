7e4ab724a6ad4706eb51489214bcaa03bb8f7608
Erik Thomsen <you@example.com>
2021-09-28 14:01:35 +0200

    tested total laxity calculation

--- Exercise/MulticoreProcessorScheduler/Program.cs
+++ Exercise/MulticoreProcessorScheduler/Program.cs
@@ -19,35 +19,64 @@ namespace MulticoreProcessorScheduler
                 Console.WriteLine(task.ToString());
             }
 
-            // print solution
-            var solution = SolutionGenerator.GetInititalSolution(tasks,processors);
-            Console.WriteLine(solution.ToString());
-
-            var neighbour = SolutionGenerator.GenerateNeighbour(solution);
-            Console.WriteLine(neighbour.ToString());
-            Console.WriteLine(solution.ToString());
-
             Console.WriteLine();
             Console.WriteLine();
 
-            AssignedTask assignedTask = new AssignedTask(tasks[0], processors[0].Cores[0], 0);
-            Console.WriteLine($"Old Wcet: {assignedTask.Wcet}, Core.WcetFactor: {assignedTask.Core.WcetFactor}, Task.Wcet: {assignedTask.Task.Wcet}");
-            assignedTask.Core = processors[0].Cores[1];
-            Console.WriteLine($"New Wcet: {assignedTask.Wcet}, Core.WcetFactor: {assignedTask.Core.WcetFactor}, Task.Wcet: {assignedTask.Task.Wcet}");
+            // // print solution
+            // var solution = SolutionGenerator.GetInititalSolution(tasks,processors);
+            // Console.WriteLine(solution.ToString());
 
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Solutions: ");
+            // var neighbour = SolutionGenerator.GenerateNeighbourMove(solution);
+            // Console.WriteLine(neighbour.ToString());
+            // Console.WriteLine(solution.ToString());
+
+            // Console.WriteLine();
+            // Console.WriteLine();
+
+            // AssignedTask assignedTask = new AssignedTask(tasks[0], processors[0].Cores[0], 0);
+            // Console.WriteLine($"Old Wcet: {assignedTask.Wcet}, Core.WcetFactor: {assignedTask.Core.WcetFactor}, Task.Wcet: {assignedTask.Task.Wcet}");
+            // assignedTask.Core = processors[0].Cores[1];
+            // Console.WriteLine($"New Wcet: {assignedTask.Wcet}, Core.WcetFactor: {assignedTask.Core.WcetFactor}, Task.Wcet: {assignedTask.Task.Wcet}");
+
+            // Console.WriteLine();
+            // Console.WriteLine();
+            // Console.WriteLine();
+            // Console.WriteLine("Solutions: ");
 
             var tuples = SimulatedAnnealing.FindOptimalSolution(tasks, processors);
             foreach (var tuple in tuples)
             {
-                //Console.WriteLine(tuple.Item1);
+                // Console.WriteLine(tuple.Item1);
             }
 
             printTuples(tuples);
 
+            /*
+            var exampleSolution = new Solution() {
+                AssignedTasks = new List<AssignedTask> {
+                    new AssignedTask(tasks[7], processors[0].Cores[0], 0),
+                    new AssignedTask(tasks[0], processors[0].Cores[1], 0),
+                    new AssignedTask(tasks[8], processors[0].Cores[2], 0),
+                    new AssignedTask(tasks[1], processors[0].Cores[3], 0),
+                    new AssignedTask(tasks[5], processors[1].Cores[0], 0),
+                    new AssignedTask(tasks[6], processors[1].Cores[1], 0),
+                    new AssignedTask(tasks[2], processors[1].Cores[2], 0),
+                    new AssignedTask(tasks[3], processors[1].Cores[2], 0),
+                    new AssignedTask(tasks[4], processors[1].Cores[3], 0),
+                }
+            };
+            exampleSolution.AssignedTasks = exampleSolution.AssignedTasks
+                .OrderBy(at => at.Task.Deadline)
+                .ThenByDescending(at => at.Wcet)
+                .ToList();
+
+
+            SimulatedAnnealing.ResponseTimeAnalysis(exampleSolution);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(exampleSolution.ToString());
+            Console.WriteLine("Total Laxity: " + SimulatedAnnealing.TotalLaxity(exampleSolution));
+            */
         }
 
         public static void printTuples(List<(double, Solution)> tuples)
